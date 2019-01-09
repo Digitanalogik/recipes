5fc237652844ff383d893ebccbdd332c505157f9
Tatu Soininen <tatu.soininen@cinia.fi>
2019-01-09 17:11:54 +0200

    Improved Ingredient.java

--- src/main/java/guru/springframework/recipes/domain/Ingredient.java
+++ src/main/java/guru/springframework/recipes/domain/Ingredient.java
@@ -13,7 +13,7 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure uom;
 
     @ManyToOne
@@ -49,5 +49,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
