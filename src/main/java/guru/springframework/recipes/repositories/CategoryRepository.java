671e9c02ec672fbf945f16aceccb40eb5c22fa83
Tatu Soininen <tatu.soininen@cinia.fi>
2019-01-09 17:36:49 +0200

    Added repositories for Category, Recipe and UnitOfMeasure

---
+++ src/main/java/guru/springframework/recipes/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.recipes.repositories;
+
+import guru.springframework.recipes.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+}
---
+++ src/main/java/guru/springframework/recipes/repositories/RecipeRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.recipes.repositories;
+
+import guru.springframework.recipes.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+
+}
